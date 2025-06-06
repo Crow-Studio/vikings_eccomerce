7e97dd46e2064e9c0a693ce21caa4306204e6875
thecodingmontana <dev.humblechris254@gmail.com>
2025-06-06 20:24:05 +0300

    feat(app): add hero section for new arrivals

---
+++ components/home/Header.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import DeliriumSvgIcon from "../svgs/DeliriumSvgIcon";
+import SolarMagniferOutline from "../svgs/SolarMagniferOutline";
+import SolarCartLarge2Outline from "../svgs/SolarCartLarge2Outline";
+import { Button } from "../ui/button";
+import { Separator } from "../ui/separator";
+import Link from "next/link";
+import SolarBagHeartOutline from "../svgs/SolarBagHeartOutline";
+
+export default function Header() {
+  return (
+    <header className="px-2 md:px-5 xl:px-10 py-3 flex items-center justify-between">
+      <h2 className="hidden md:block text-sm text-muted-foreground">
+        Free shipping and 30 days return
+      </h2>
+      <div className="flex items-center gap-1.5">
+        <DeliriumSvgIcon className="w-7 h-auto" />
+        <h1 className="text-xl font-semibold text-[#353535]">Delirium</h1>
+      </div>
+      <div className="flex h-5 items-center space-x-2 md:space-x-3 text-sm">
+        <Button
+          size="icon"
+          variant="ghost"
+          className="rounded-full cursor-pointer"
+        >
+          <SolarMagniferOutline />
+        </Button>
+        <Button
+          size="icon"
+          variant="ghost"
+          className="rounded-full cursor-pointer"
+        >
+          <SolarCartLarge2Outline />
+        </Button>
+        <Button
+          size="icon"
+          variant="ghost"
+          className="rounded-full cursor-pointer"
+        >
+          <SolarBagHeartOutline />
+        </Button>
+        <Separator orientation="vertical" />
+        <Link href="/signin">Sign in</Link>
+      </div>
+    </header>
+  );
+}
---
+++ components/home/Hero.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import SolarTShirtOutline from "../svgs/SolarTShirtOutline";
+import { Separator } from "../ui/separator";
+import StreamlinePayment10 from "../svgs/StreamlinePayment10";
+import NewArrivals from "./NewArrivals";
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+export default function Hero() {
+  return (
+    <div className="grid grid-cols-2 gap-[6rem] p-10">
+      <div className="space-y-16">
+        <h2 className="text-5xl text-balance">
+          Our newest arrivals are here to help you look your best.
+        </h2>
+        <div className="grid gap-8">
+          <div className="flex items-center h-10 gap-x-4">
+            <div className="flex items-center gap-x-2">
+              <SolarTShirtOutline className="self-start size-5 mt-1 font-medium" />
+              <div className="self-start">
+                <h3 className="uppercase font-semibold">Quality Materials</h3>
+                <p className="text-muted-foreground text-sm">
+                  Essentials crafted to elevate your everyday.
+                </p>
+              </div>
+            </div>
+            <Separator orientation="vertical" className="h-full" />
+            <div className="flex items-center gap-x-4">
+              <StreamlinePayment10 className="self-start size-5 mt-1 font-medium" />
+              <div className="self-start">
+                <h3 className="uppercase font-semibold">Secure Payment</h3>
+                <p className="text-muted-foreground text-sm">
+                  Guarantee 100% secure payment online on our website.
+                </p>
+              </div>
+            </div>
+          </div>
+          <Link href="/" className="group cursor-pointer">
+            <Button className="cursor-pointer">
+              Browse our collection
+              <div className="size-6 overflow-hidden rounded-full duration-500">
+                <div className="flex w-12 -translate-x-1/2 duration-500 ease-in-out group-hover:translate-x-0">
+                  <span className="flex size-6">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="lucide lucide-arrow-right m-auto size-[14px]"
+                      aria-hidden="true"
+                    >
+                      <path d="M5 12h14"></path>
+                      <path d="m12 5 7 7-7 7"></path>
+                    </svg>
+                  </span>
+                  <span className="flex size-6">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="lucide lucide-arrow-right m-auto size-[14px]"
+                      aria-hidden="true"
+                    >
+                      <path d="M5 12h14"></path>
+                      <path d="m12 5 7 7-7 7"></path>
+                    </svg>
+                  </span>
+                </div>
+              </div>
+            </Button>
+          </Link>
+        </div>
+      </div>
+      <NewArrivals />
+    </div>
+  );
+}
