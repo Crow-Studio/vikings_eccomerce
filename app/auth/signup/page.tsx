7142901c78c33ba5e20af4eb67540252858c6576
thecodingmontana <dev.humblechris254@gmail.com>
2025-06-09 12:42:47 +0300

    fix(app): auth pages have an invalid export

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { cn } from "@/lib/utils";
 import { SignupForm } from "@/components/auth/SignupForm";
 
-export default function SignupPage({ className }: React.ComponentProps<"div">) {
+export default function SignupPage() {
   return (
-    <div className={cn("flex flex-col gap-6", className)}>
+    <div className="flex flex-col gap-6">
       <SignupForm />
       <div className="text-muted-foreground z-10 *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
         By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
