602b5d4ee3e1434ad6a8bf32fe3ed42b1ba92281
thecodingmontana <dev.humblechris254@gmail.com>
2025-09-16 13:15:19 +0300

    ci(app): updated ecosystem.config.cjs and github actions

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,11 +2,16 @@ module.exports = {
   apps: [
     {
       name: "vikingstraders",
-      script: "pnpm",
+      script: "node_modules/.bin/next",
       args: "start",
       instances: 2,
       exec_mode: "cluster",
       watch: false,
+      cwd: "/var/www/vikings",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
       env_file: ".env.production",
       log_date_format: "YYYY-MM-DD HH:mm:ss",
       error_file: "/var/log/pm2/vikingstraders-error.log",
@@ -15,6 +20,9 @@ module.exports = {
       combine_logs: true,
       time: true,
       max_memory_restart: "1G",
+      max_restarts: 3,
+      min_uptime: "10s",
+      restart_delay: 4000,
     },
   ],
 };
