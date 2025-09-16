4ea340ef83e354488572bdb23f8e142e79617d0f
thecodingmontana <dev.humblechris254@gmail.com>
2025-09-16 09:59:20 +0300

    ci(app): change drizzle .env settings

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,8 @@
 import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
-config({ path: ".env" });
+
+config({ path: ".env.production" });
+
 export default defineConfig({
   schema: "./database/schema.ts",
   out: "./database/migrations",
