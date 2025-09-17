97250538744584e614768062bb023519d754ee69
thecodingmontana <dev.humblechris254@gmail.com>
2025-09-17 23:16:29 +0300

    fix(app): eslint and type checks errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,8 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-empty-object-type": "off",
       "@typescript-eslint/no-explicit-any": "off",
-      "react-hooks/exhaustive-deps": "off"
+      "react-hooks/exhaustive-deps": "off",
+      "@next/next/no-img-element": "off"
     },
   }),
 ];
