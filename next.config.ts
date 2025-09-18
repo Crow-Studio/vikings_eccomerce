eb4aa230b9bedd6d0f3feb2e8f7d80f5b4156592
thecodingmontana <dev.humblechris254@gmail.com>
2025-09-18 22:11:16 +0300

    fix(app): cloudinary side effect

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /cloudinary/,
+      sideEffects: true,
+    });
+    return config;
+  },
   images: {
     remotePatterns: [
       {
