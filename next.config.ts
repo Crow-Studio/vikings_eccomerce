3551e7c54884542cd18808124dfbc405688b9769
thecodingmontana <dev.humblechris254@gmail.com>
2025-09-18 20:08:46 +0300

    chore(app): add console log messages

--- next.config.ts
+++ next.config.ts
@@ -25,9 +25,9 @@ const nextConfig: NextConfig = {
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
-  },
+  // compiler: {
+  //   removeConsole: process.env.NODE_ENV === "production",
+  // },
   serverExternalPackages: ["@node-rs/argon2"],
 };
 
